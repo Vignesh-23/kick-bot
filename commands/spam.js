5d85b38df129ffd32d069d61b6e77eba91dc15d8
Vignesh <psvishal23@gmail.com>
2021-09-12 23:10:54 +0530

    added spam functionality that spamms the user and deletes the message after

---
+++ commands/spam.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const data = new SlashCommandBuilder()
+    .setName('spam')
+    .setDescription('spam @user <number> spams the specified user by mentioning them for specified number of times')
+    .addUserOption(option =>
+        option.setName('target')
+            .setDescription('user to spam')
+            .setRequired(true))
+    .addNumberOption(option =>
+        option.setName('seconds')
+            .setDescription('Time in seconds the user is spammed (capped at 20 and defaulted at 10)'));
+
+module.exports = {
+	data: data,
+	async execute(interaction) {
+        // if (interaction.user.id != '575578770568773632') {
+        //     await interaction.reply(`Only PUDDIN can do this action!`)
+        // }
+
+        const targetUser = interaction.options.getMember('target');
+        var seconds = interaction.options.getNumber('seconds');
+
+        if ( seconds ) {
+            if (seconds > 20) seconds = 20;
+            if (seconds < 0 ) seconds = 0;
+        } else {
+            seconds = 10;
+        }
+
+        // continuously send disconnect request for specified number of seconds
+        const startTime = new Date();
+        const stopInterval = setInterval(async () => {
+            if (new Date().getTime() - startTime.getTime() > seconds*1000) {
+                clearInterval(stopInterval);
+            }
+            
+            try {
+                interaction.channel.send(`${targetUser.user} sup biyatch`)
+                    .then(msg => {
+                        msg.delete({timeout: "2000"});
+                    })
+                    .catch();
+
+            } catch(error) {
+    
+            }
+            
+        }, 1000);
+    
+		await interaction.reply(`spamming ${targetUser.user} for ${seconds} seconds`);
+	},
+};
--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setActivity("Vaathi in the A$$", {type: "Kicking"});
+		client.user.setActivity("to Vaathi screaming ", {type: "LISTENING"});
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
